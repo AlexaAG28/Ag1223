1e3fd0a6f18e23df328d62af17393eda1ab892c7
stivenjv7 <66438457+stivenjv7@users.noreply.github.com>
2020-09-15 17:28:26 -0500

    CODIGO
    ENVIO Y PARTIDA

---
+++ BACOMADE.java
@@ -0,0 +1,27 @@
+
+package bacomade;
+
+public class BACOMADE {
+    int IDEnvio;
+    String FechaEnvio;
+    
+    public int getIDEnvio () {
+        return IDEnvio;
+    }
+    
+    public String getFechaEnvio () {
+        return FechaEnvio;
+    }
+
+    
+    public  void setIDEnvio(int IDEnvio) {
+        this.IDEnvio = IDEnvio;
+    }
+    
+    
+     public  void setFechaEnvio(String FechaEnvio) {
+        this.FechaEnvio = FechaEnvio;
+    }
+    
+}
+
---
+++ PARTIDA.java
@@ -0,0 +1,50 @@
+
+package bacomade;
+
+
+public class PARTIDA {
+    int IDPartida;
+    String Tipo;
+    String FechaCreada;
+    String Costo;
+    
+    
+    public int getIDPartida () {
+        return IDPartida;
+    }
+    
+    public String getTipo () {
+        return Tipo;
+    }
+    
+   
+    
+    public String getFechaCreada () {
+        return FechaCreada;
+    }
+    
+     public String getCosto () {
+        return Costo;
+    }
+
+    
+    public  void setIDPartida(int IDPartida) {
+        this.IDPartida = IDPartida;
+    }
+    
+    
+     public  void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+     
+    public  void setFechaCreada(String FechaCreada) {
+        this.FechaCreada = FechaCreada;
+    } 
+    
+    public  void setCosto(String Costo) {
+        this.Costo = Costo;
+    }
+    
+}
+
+
